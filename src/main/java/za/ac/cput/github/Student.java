d6a4693c4c3ef81f68573d7d97eccc86cba202da
Chad Siegelaar <chadsiegelaar20@gmail.com>
2021-04-01 23:07:11 +0200

    Added coding to Student class

--- src/main/java/za/ac/cput/github/Student.java
+++ src/main/java/za/ac/cput/github/Student.java
@@ -7,8 +7,41 @@ package za.ac.cput.github;
 
 /**
  *
- * @author chadsiegelaar
+ * @author Chad Siegelaar 218340982
  */
 public class Student {
+  
+    private String name, studentGrade;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentGrade() {
+        return studentGrade;
+    }
+
+    public void setStudentGrade(String studentGrade) {
+        this.studentGrade = studentGrade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", studentGrade=" + studentGrade + ", id=" + id + '}';
+    }
+
     
 }
